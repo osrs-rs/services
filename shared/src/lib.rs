386e4c59918a3a159d4939a90c54b8171f6eb026
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-02-18 12:04:06 +0100

    Added sample of playerdata and serialization of it

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,8 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PlayerData {
+    pub skills: HashMap<u8, Skill>,
+    pub inventories: HashMap<u16, Inventory>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Skill {
+    pub level: i8,
+    pub xp: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Inventory {
+    pub items: HashMap<u16, Item>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    pub id: i32,
+    pub amount: i32,
 }
